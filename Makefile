c881e2bf5dd9ab8c2fb899d6e26d61731766c86f
arnav-gudibande <arnav.gudibande@gmail.com>
2016-08-03 10:15:59 -0700

    Redis fetch pitch & yaw

--- Makefile
+++ Makefile
@@ -4,10 +4,10 @@ CFLAGS+=-D_LINUX
 LBITS := $(shell getconf LONG_BIT)
 ifeq ($(LBITS),64)
 s826demo: s826_example.o lib826_64.a
-	$(CC) $(CFLAGS) s826_example.o -o s826demo -lm -L./ -l826_64 -L./hiredis -lhiredis -g
+	$(CC) $(CFLAGS) s826_example.o -o s826demo -lm -L./ -l826_64 -L./hiredis -lhiredis
 else
 s826demo: s826_example.o lib826.a
-	$(CC) $(CFLAGS) s826_example.o -o s826demo -lm -L./ -l826 -L./hiredis -lhiredis
+	$(CC) $(CFLAGS) s826_example.o -o s826demo -lm -L./ -l826
 endif
 
 all: s826demo
@@ -16,8 +16,8 @@ all: s826demo
 
 
 s826_example.o: s826_example.c
-	$(CC) $(CFLAGS)  -Wall -Wextra -DOSTYPE_LINUX -c  s826_example.c -o s826_example.o -L./hiredis -lhiredis -g
 
+	$(CC) $(CFLAGS)  -Wall -Wextra -DOSTYPE_LINUX -c  s826_example.c -o s826_example.o -L./hiredis -lhiredis
 
 clean:
 	rm -rf s826_example.o s826demo
--- s826_example.c
+++ s826_example.c
@@ -46,13 +46,17 @@ static int NRCstart(uint board)
     }
 
     X826( S826_SafeWrenWrite(board, 2) );
-    X826( S826_DacRangeWrite(board, 0, S826_DAC_SPAN_5_5, safemode)       );  // program dac output range: -10V to +10V
+    X826( S826_DacRangeWrite(board, 0, S826_DAC_SPAN_5_5, safemode) );  // program dac output range: -10V to +10V
     X826( S826_DacRangeWrite(board, 1, S826_DAC_SPAN_5_5, safemode) );
     X826( S826_DacRangeWrite(board, 2, S826_DAC_SPAN_5_5, safemode) );
 
     while(1) {
       redisReply *y = redisCommand(c, "GET yaw");
       X826( S826_DacDataWrite(board, 2, strtoul(y->str, 0, 0), safemode) );
+
+      redisReply *p = redisCommand(c, "GET pitch");
+      X826( S826_DacDataWrite(board, 0, strtoul(p->str, 0, 0), safemode) );
+      printf("%s\n", p->str);
     }
 
     return errcode;
@@ -77,7 +81,6 @@ int main(int argc, char **argv)
             }
         }
     } else  {
-        printf("Here");
         X826( NRCstart(board) );
     }
 
